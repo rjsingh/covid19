6925f77f55bb3683f15418640cc0f1dda4a9be1a
rs <rs@DESKTOP-CPOM8HF.localdomain>
2020-03-25 08:29:46 +0000

    Expose port in development environment.
    - Wrong host being sent requests.

--- react-corona-app/src/CoronaCasesTable.js
+++ react-corona-app/src/CoronaCasesTable.js
@@ -23,9 +23,8 @@ class CoronaCasesTable extends React.Component {
   }
 
   loadData(location=null) {
-    // Fetching data from FaceBook Jest Repo
     fetch(
-      'http://localhost/location/' + location,
+      '/location/' + location,
       {
         method: "GET",
       }
